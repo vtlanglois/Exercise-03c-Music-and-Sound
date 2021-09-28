bd1d58e062d5ca153edd2103e2ee64d93684d11d
BL-MSCH-C220-F19 <53633703+BL-MSCH-C220-F19@users.noreply.github.com>
2021-09-28 11:40:24 -0400

    Initial commit

---
+++ Pieces/Piece.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+export (String) var color
+var is_matched
+var is_counted
+var selected = false
+var target_position = Vector2.ZERO
+
+var Effects = null
+var dying = false
+var wiggle = 0.0
+export var wiggle_amount = 3
+
+export var transparent_time = 1.0
+export var scale_time = 1.5
+export var rot_time = 1.5
+
+var Coin = preload("res://Coin.tscn")
+
+
+func _ready():
+	$Select.texture = $Sprite.texture
+	$Select.scale = $Sprite.scale
+
+func _physics_process(_delta):
+	if dying:
+		queue_free()
+	elif position != target_position:
+		position = target_position
+	if selected:
+		$Select.show()
+		$Selected.emitting = true
+	else:
+		$Select.hide()
+		$Selected.emitting = false
+
+func generate(pos):
+	position = Vector2(pos.x,-100)
+	target_position = pos
+
+func move_piece(change):
+	target_position = target_position + change
+
+func die():
+	dying = true;
+	if Effects == null:
+		Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var coin = Coin.instance()
+		coin.position = position
+		Effects.add_child(coin)
+	
