0a9b936b0ac1419d2dfa256ccb4762aa3e48520d
Langlois, Vince Thomas <vtlanglo@iu.edu>
2021-09-29 22:02:35 -0400

    done

--- Pieces/Piece.gd
+++ Pieces/Piece.gd
@@ -15,8 +15,12 @@ export var transparent_time = 1.0
 export var scale_time = 1.5
 export var rot_time = 1.5
 
+var sound_1 = null;
+var sound_2 = null;
+var sound_3 = null;
 
 
+var Coin = preload("res://Coin.tscn")
 func _ready():
 	$Select.texture = $Sprite.texture
 	$Select.scale = $Sprite.scale
@@ -36,9 +40,28 @@ func _physics_process(_delta):
 func generate(pos):
 	position = Vector2(pos.x,-100)
 	target_position = pos
+	if sound_3 == null:
+		sound_3 = get_node_or_null("/root/Game/3")
+	if sound_3 != null:
+		sound_3.play()
+	
 
 func move_piece(change):
 	target_position = target_position + change
+	if sound_2 == null:
+		sound_2 = get_node_or_null("/root/Game/2")
+	if sound_2 != null:
+		sound_2.play()
 
 func die():
 	dying = true;
+	if sound_1 == null:
+		sound_1 = get_node_or_null("/root/Game/1")
+	if sound_1 != null:
+		sound_1.play()
+	if Effects == null:
+		Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var coin = Coin.instance()
+		coin.position = target_position
+		Effects.add_child(coin)
---
+++ UI/Music.gd
@@ -0,0 +1,7 @@
+extends AudioStreamPlayer
+
+
+
+
+func _on_Music_finished():
+	play()
